6df2d575374801e5eb3c88e95eb54ed60d15477e
whrrmydragons <omrihaber@me.com>
2020-02-23 00:10:14 +0200

    wip

--- lazystocks/providers/stock/alpha_vantage/provider.py
+++ lazystocks/providers/stock/alpha_vantage/provider.py
@@ -1,23 +1,27 @@
 import requests
 import pandas as pd
+import re
 
 
 def url(api_key, symbol):
     return "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol="+symbol+"&apikey="+api_key
 
 
-def get_raw_data(api_key, symbol="SPY"):
+def get_raw_data(api_key, symbol):
     res = requests.get(url=url(api_key, symbol)).json()
-    print(res)
     return res
 
 
 def transform_data_to_df_normal_form(data):
-    df = pd.DataFrame(data)
-    # TODO: cleand and transform to normal form
+    daily_data = data["Time Series (Daily)"]
+    df = pd.DataFrame(daily_data)
+    df = df.T #each row should be a sample
+    remove_numbers_and_spaces = lambda s:re.sub("(\d*)([\.]*)(\s*)", "", s)
+    df = df.rename(remove_numbers_and_spaces, axis='columns')
+    # TODO: clean and transform to normal form
     return df
 
 
-def get_df():
-    data = get_raw_data(api)
-    return transform_df_to_normal_form(data)
+def get_df(api_key, symbol="SPY"):
+    data = get_raw_data(api_key, symbol)
+    return transform_data_to_df_normal_form(data)
